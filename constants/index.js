402523b66a3d2faccf88e0ccd1cf14fbab95ed86
Aman Porwal <amanporwal510@gmail.com>
2022-12-11 20:23:16 +0530

    add constant folder for form initialvalues

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+export const initialFormValues = {
+   email: '',
+   name: '',
+   age: '',
+   batchTiming: null,
+   isEmailContainsError: false,
+   isAgeContainsError: false,
+   emailErrorText: '',
+   ageErrorText: ''
+};
